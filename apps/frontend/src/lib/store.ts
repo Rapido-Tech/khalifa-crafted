ae018de67401fcad8355808e038d56f40c493a1f
Homboi <rapidotechdev@gmail.com>
2026-05-22 01:15:32 +0300

    fix(frontend): import combineReducers from @reduxjs/toolkit not redux (pnpm peer dep)

--- apps/frontend/src/lib/store.ts
+++ apps/frontend/src/lib/store.ts
@@ -1,5 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import { useSelector, useDispatch } from "react-redux";
 import storage from "redux-persist/lib/storage";
 import {
